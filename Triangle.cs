02b18dcab403a9a263c42bdd9a9c9a04e9dcc223
KristofferSaxmo <54435541+KristofferSaxmo@users.noreply.github.com>
2020-08-26 09:22:14 +0200

    Add files via upload

---
+++ Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Triangle : Shape
+    {
+        public Triangle(int height, int width) : base(height, width)
+        {
+            
+        }
+    }
+}
